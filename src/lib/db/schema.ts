e6b3fffa39d1dbee67624c6016e0e0ab21a7c162
sukalov <matvey10@gmail.com>
2023-07-11 05:08:05 +0300

    .

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,11 +1,6 @@
 import { mysqlTable, serial, text, int, date, decimal } from 'drizzle-orm/mysql-core'
 
-/**
- * This is a sample schema.
- * Replace this with your own schema and then run migrations.
- */
-
-export const stock = mysqlTable('stock', {
+export const stocks_data = mysqlTable('stocks_data', {
   id: serial('id').primaryKey(),
   symbol: text('symbol'),
   currency: text('currency'),
@@ -17,4 +12,15 @@ export const stock = mysqlTable('stock', {
   low: decimal('low'),
   close: decimal('close'),
   volume: int('volume'),
+})
+
+export const stocks_info = mysqlTable('stocks_info', {
+  id: serial('id').primaryKey(),
+  symbol: text('symbol'),
+  name: text('name'),
+  currency: text('currency'),
+  exchange: text('exchange'),
+  mic_code: text('mic_code'),
+  country: text('country'),
+  type: text('type'),
 })
