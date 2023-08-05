b90c7fee88489ad680c2348c39f829d18422a1f0
sukalov <matvey10@gmail.com>
2023-08-06 02:57:45 +0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,10 @@ import { GlobalNav } from '@/components/global-nav';
 export default function Home() {
   const indicies = ['kpop-25', 'cosmetics-15', 'consumer-50']
   return (
-    <>
+    <div className='p-8 text-muted-foreground'>
     {
     indicies.map(indexName => (
-      <div key={indexName} className="gap-3 items-center justify-left p-8 text-muted-foreground">
+      <div key={indexName}>
         <Link href={`/api/stocks-info/${indexName}`}>
           <Button variant={'link'}>info {indexName}</Button>
         </Link><br />
@@ -26,26 +26,6 @@ export default function Home() {
       </div>
     ))
   }
-    {/* <div className="h-[calc(100vh-4rem)] gap-3 items-center justify-left p-8 min-w-screen text-muted-foreground">
-      <Link href={'/api/stocks-info/kpop-25'}>
-        <Button variant={'link'}>info kpop-25</Button>
-      </Link><br />
-      <Link href={'/api/stocks-info/cosmetics-15'}>
-        <Button variant={'link'}>info cosmetics-15</Button>
-      </Link><br />
-      <Link href={'/api/adjustments/kpop-25'}>
-        <Button variant={'link'}>adjustments kpop-25</Button>
-      </Link><br />
-      <Link href={'/api/adjustments/cosmetics-15'}>
-        <Button variant={'link'}>adjustments cosmetics-15</Button>
-      </Link><br />
-      <Link href={'/api/index/kpop-25'}>
-        <Button variant={'link'}>index kpop-25</Button>
-      </Link><br />
-      <Link href={'/api/index/cosmetics-15'}>
-        <Button variant={'link'}>index cosmetics-15</Button>
-      </Link><br />
-    </div> */}
-    </>
+    </div>
   );
 }
