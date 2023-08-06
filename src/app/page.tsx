0d2732503fbbdac3d19545dc5c78e267055175ce
sukalov <matvey10@gmail.com>
2023-08-06 03:05:11 +0300

    main page frontend edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,18 +10,19 @@ import { GlobalNav } from '@/components/global-nav';
 export default function Home() {
   const indicies = ['kpop-25', 'cosmetics-15', 'consumer-50']
   return (
-    <div className='p-8 text-muted-foreground'>
+    <div className='p-8 text-muted-foreground table'>
     {
     indicies.map(indexName => (
-      <div key={indexName}>
+      <div key={indexName} className='pb-6'>
+        <p className=' text-lg p-0'>{indexName}</p>
         <Link href={`/api/stocks-info/${indexName}`}>
-          <Button variant={'link'}>info {indexName}</Button>
+          <Button variant={'link'}>info</Button>
         </Link><br />
         <Link href={`/api/adjustments/${indexName}`}>
-          <Button variant={'link'}>adjustments {indexName}</Button>
+          <Button variant={'link'}>adjustments</Button>
         </Link><br />
         <Link href={`/api/index/${indexName}`}>
-          <Button variant={'link'}>index {indexName}</Button>
+          <Button variant={'link'}>index</Button>
         </Link>
       </div>
     ))
