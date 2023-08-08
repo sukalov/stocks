249d6553942f42cefefd1cf81c0946d42faf4906
sukalov <matvey10@gmail.com>
2023-08-08 18:10:35 +0300

    force dynamic reload of table

--- src/app/stocks-info/page.tsx
+++ src/app/stocks-info/page.tsx
@@ -3,8 +3,10 @@ import { StocksInfoTable } from "@/components/stocks-info-table";
 import { db } from '@/lib/db';
 import { stocks_info } from '@/lib/db/schema';
 
+export const dynamic = 'force-dynamic';
+
 export default async function MarketData() {
-  const stocks = await db.select().from(stocks_info).orderBy(stocks_info.indicies) as StockInfo[]
+  const stocks = await db.select().from(stocks_info).orderBy(stocks_info.indicies) as StockInfo[] 
   return (
     <div className="mb-16">
       <StocksInfoTable data={stocks} />
