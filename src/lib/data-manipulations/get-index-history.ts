cdb2b601b64aa25f49126b9a87b1c64670e0aa69
sukalov <matvey10@gmail.com>
2023-08-16 17:53:35 +0300

    correct index counting

--- src/lib/data-manipulations/get-index-history.ts
+++ src/lib/data-manipulations/get-index-history.ts
@@ -3,6 +3,8 @@ export default function getIndexHistory (dataIndexPrices: any, dataAdjustments: 
       dataIndexPrices.shift();
     }
     let baseIndexPrice = 0;
+    let basePercent = 100;
+    let switchDay = false;
     let i = 0;
     let indexHistory: { date: any; adjustment: any; index_price: number; index: number; name: string }[] = [];
     dataIndexPrices.forEach((day: any, ind: number) => {
@@ -16,22 +18,35 @@ export default function getIndexHistory (dataIndexPrices: any, dataAdjustments: 
       } else {
         checkAdjDate = tomorrow;
       }
-      if (dayDate.toLocaleDateString() === checkAdjDate.toLocaleDateString()) i += 1;
+      if (dayDate.toLocaleDateString() === checkAdjDate.toLocaleDateString()) {
+        switchDay = true;
+        i += 1;
+        const percents = dataAdjustments[i - 1].percents;
+          let index_price = 0;
+          Object.keys(percents).forEach((symbol) => {
+        index_price += day[symbol] * percents[symbol];
+      });
+      // console.log(`${index_price}, ${baseIndexPrice}, ${basePercent}`)
+      basePercent = (index_price / baseIndexPrice) * basePercent
+      }
 
       const percents = dataAdjustments[i].percents;
       let index_price = 0;
       Object.keys(percents).forEach((symbol) => {
         index_price += day[symbol] * percents[symbol];
       });
-      if (ind === 0) baseIndexPrice = index_price;
-      //
+      if (ind === 0 || switchDay) {
+        baseIndexPrice = index_price;
+        switchDay = false
+      }
 
       indexHistory.push({
         date: day.date,
         name: indexName,
         adjustment: dataAdjustments[i].date.toISOString().slice(0, 10),
         index_price,
-        index: (index_price / baseIndexPrice) * 100,
+        index: (index_price / baseIndexPrice) * basePercent,
+        // check: `${index_price}, ${baseIndexPrice}, ${basePercent}`,
       });
     });
 
