f429e01a488e651d5c45ed20b222ab42d1e7dbec
sukalov <matvey10@gmail.com>
2023-08-31 18:53:35 +0300

    ..

--- src/app/api/update-currencies/route.ts
+++ src/app/api/update-currencies/route.ts
@@ -3,7 +3,7 @@ import { initialSteps } from '@/lib/data-manipulations/update-currencies-data';
 export async function GET(request: any, context: any) {
   // const newData = initialSteps();
 
-  return new Response(JSON.stringify(newData), {
+  return new Response(JSON.stringify([]), {
     status: 200,
     headers: {
       'Content-Type': 'text/json',
