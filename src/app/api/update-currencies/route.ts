cf7635d9bda87a5f2158699af406251dd8c7cbd3
sukalov <matvey10@gmail.com>
2023-10-23 13:41:44 +0300

    suspend upd currencies

--- src/app/api/update-currencies/route.ts
+++ src/app/api/update-currencies/route.ts
@@ -1,9 +1,9 @@
 import { initialSteps } from '@/lib/data-manipulations/update-currencies-data';
 
 export async function GET(request: any, context: any) {
-  const newData = await initialSteps();
+  // const newData = await initialSteps();
 
-  return new Response(JSON.stringify(newData), {
+  return new Response(JSON.stringify('newData'), {
     status: 200,
     headers: {
       'Content-Type': 'text/json',
