41f04f5227fa3f71b02b839f38f0e5d45efcd3f1
sukalov <matvey10@gmail.com>
2023-12-20 20:34:28 -0700

    suspend "update-everything" endpoint

--- src/app/api/update-everything/route.ts
+++ src/app/api/update-everything/route.ts
@@ -13,7 +13,7 @@ import { updateMarketCaps } from '@/lib/data-manipulations/update-market-caps';
 import { csv } from '@/lib/read-write-csv';
 
 export async function GET(request: any, context: any) {
-  await initialSteps();
+  // await initialSteps();
   // const last_date = await db
   //   .select()
   //   .from(currencies)
@@ -30,63 +30,63 @@ export async function GET(request: any, context: any) {
   //   //  ) as DataSharesOutstanding[];
   //   .where(sql`JSON_CONTAINS(${stocks_info.indicies}, ${nameForSQL})`)) as DataSharesOutstanding[];
 
-  const dataSharesOutstanding = (await db
-    .select()
-    .from(stocks_info)
-    // .where(inArray(stocks_info.symbol, ['5574.TSE', '420770.KQ', '408900.KO', '5253.TSE', '439090.KQ', '6757.TW', '9166.TSE', '6757.TW', '406820.KQ']))
-    // .where(inArray(stocks_info.symbol, ['5574.TSE', '420770.KQ']))
-    ) as StocksInfo[];
-    const dataSharesOutstandingNoDelisted = (await db
-      .select()
-      .from(stocks_info)
-      .where(isNull(stocks_info.is_delisted))
-      ) as StocksInfo[];
+  // const dataSharesOutstanding = (await db
+  //   .select()
+  //   .from(stocks_info)
+  //   // .where(inArray(stocks_info.symbol, ['5574.TSE', '420770.KQ', '408900.KO', '5253.TSE', '439090.KQ', '6757.TW', '9166.TSE', '6757.TW', '406820.KQ']))
+  //   // .where(inArray(stocks_info.symbol, ['5574.TSE', '420770.KQ']))
+  //   ) as StocksInfo[];
+  //   const dataSharesOutstandingNoDelisted = (await db
+  //     .select()
+  //     .from(stocks_info)
+  //     .where(isNull(stocks_info.is_delisted))
+  //     ) as StocksInfo[];
 
 
-  const currData = (await db.select().from(currencies)) as CurrenciesPrice[];
-  const dbDataDividents = await db.select().from(dividents);
-  const dataDividents = dbDataDividents.reduce((prev: DataDividents, curr: any) => {
-    const date = curr.date.toISOString().slice(0, 10);
-    prev[date] = curr.dividents;
-    return prev;
-  }, {});
+  // const currData = (await db.select().from(currencies)) as CurrenciesPrice[];
+  // const dbDataDividents = await db.select().from(dividents);
+  // const dataDividents = dbDataDividents.reduce((prev: DataDividents, curr: any) => {
+  //   const date = curr.date.toISOString().slice(0, 10);
+  //   prev[date] = curr.dividents;
+  //   return prev;
+  // }, {});
 
   // const dataIndexPrices = await csv.readJSON('indexPrices')
 
-  const dataIndexPrices = await getIndexPrices(dataSharesOutstanding, currData, '2022-12-28');
-  await csv.writeJSON('indexPrices', dataIndexPrices)
-  await db.delete(indexprices) //.where(eq(indexprices.type, 'indexprices'))
-  await timeout(1000)
-  await db.insert(indexprices).values({type: 'indexprices', json: dataIndexPrices})
+  // const dataIndexPrices = await getIndexPrices(dataSharesOutstanding, currData, '2022-12-28');
+  // await csv.writeJSON('indexPrices', dataIndexPrices)
+  // await db.delete(indexprices) //.where(eq(indexprices.type, 'indexprices'))
+  // await timeout(1000)
+  // await db.insert(indexprices).values({type: 'indexprices', json: dataIndexPrices})
 
   // const dataIndexPricesDB = await db.select().from(indexprices)
   // const dataIndexPrices = dataIndexPricesDB[0]?.json as any[]
   // await db.delete(indicies)
 
   
-  for (let i = 0; i < indexNames.length; i++) {
-    const indexName = String(indexNames[i]);
-    const oldAdjustments = await db
-      .select()
-      .from(adjustments)
-      .where(eq(adjustments.index, indexName))
+  // for (let i = 0; i < indexNames.length; i++) {
+  //   const indexName = String(indexNames[i]);
+  //   const oldAdjustments = await db
+  //     .select()
+  //     .from(adjustments)
+  //     .where(eq(adjustments.index, indexName))
 
-    oldAdjustments.sort(function(a,b){
-      return new Date(b.date) + new Date(a.date);
-    });
+  //   oldAdjustments.sort(function(a,b){
+  //     return new Date(b.date) + new Date(a.date);
+  //   });
 
-    const indexHistory = getIndexHistory2(dataIndexPrices, oldAdjustments, dataDividents, indexName) as any[];
-    newData = [...newData, ...indexHistory];
-    await db.delete(indicies).where(eq(indicies.name, indexName));
-    await db.insert(indicies).values(indexHistory);
-    console.log({indexName, status: 'done'})
-  }
+  //   const indexHistory = getIndexHistory2(dataIndexPrices, oldAdjustments, dataDividents, indexName) as any[];
+  //   newData = [...newData, ...indexHistory];
+  //   await db.delete(indicies).where(eq(indicies.name, indexName));
+  //   await db.insert(indicies).values(indexHistory);
+  //   console.log({indexName, status: 'done'})
+  // }
 
-  await updateMarketCaps(dataSharesOutstandingNoDelisted, dataIndexPrices);
+  // await updateMarketCaps(dataSharesOutstandingNoDelisted, dataIndexPrices);
 
   // await db.delete(indicies)
 
-  return new Response(JSON.stringify(newData), {
+  return new Response('endpoint unavailible for public usage', {
     status: 200,
     headers: {
       'Content-Type': 'text/json',
